1d06eb0b9db5806e0a41007ed3d29ff5f20ab96f
Don <iamshixudong@gmail.com>
2024-01-24 17:33:57 +0100

    Demo to use RMA

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args[1].as_str() {
-        "mpi" => use_mpi::use_mpi(),
+        "mpi" => use_mpi::try_rma(),
         "zmq" => {
             match args[2].as_str() {
                 "res" => use_zeromq::use_zeromq_responder(),
--- src/use_mpi.rs
+++ src/use_mpi.rs
@@ -1,6 +1,7 @@
 use mpi_sys::*;
 use std::ffi::CStr;
-use std::os::raw::{c_char, c_int};
+use std::os::raw::{c_char, c_int, c_void};
+use std::ptr;
 
 pub fn use_mpi() {
     unsafe {
@@ -34,6 +35,67 @@ pub fn use_mpi() {
     );
 
     unsafe {
+        MPI_Finalize();
+    }
+}
+
+pub fn try_rma() {
+    unsafe {
+        // Initialize MPI
+        MPI_Init(ptr::null_mut(), ptr::null_mut());
+
+        // Get the rank and size
+        let mut rank: c_int = 0;
+        let mut size: c_int = 0;
+        MPI_Comm_rank(RSMPI_COMM_WORLD, &mut rank);
+        MPI_Comm_size(RSMPI_COMM_WORLD, &mut size);
+
+        if size < 2 {
+            eprintln!("This program requires at least two processes");
+            MPI_Abort(RSMPI_COMM_WORLD, 1);
+        }
+
+        let mut win: MPI_Win = ptr::null_mut();
+        let mut data: c_int = if rank == 0 { 123 } else { 0 };
+
+        // Create an MPI window
+        MPI_Win_create(
+            &mut data as *mut _ as *mut c_void,
+            std::mem::size_of::<c_int>() as MPI_Aint,
+            std::mem::size_of::<c_int>() as c_int,
+            RSMPI_INFO_NULL,
+            RSMPI_COMM_WORLD,
+            &mut win,
+        );
+
+        // Synchronize
+        MPI_Win_fence(0, win);
+
+        if rank == 0 {
+            // Rank 0 sends data to Rank 1
+            let target_rank = 1;
+            MPI_Put(
+                &data as *const _ as *const c_void,
+                1,
+                RSMPI_INT32_T,
+                target_rank,
+                0,
+                1,
+                RSMPI_INT32_T,
+                win,
+            );
+        }
+
+        // Another synchronization
+        MPI_Win_fence(0, win);
+
+        if rank == 1 {
+            // Rank 1 prints the received data
+            println!("Rank 1 received data: {}", data);
+        }
+
+        // Clean up
+        MPI_Win_free(&mut win);
         MPI_Finalize();
     }
 }
