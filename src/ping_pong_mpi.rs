a884265386c141978931c2e794cdfe4cbca36cfd
Don <iamshixudong@gmail.com>
2024-02-23 21:44:33 +0100

    Can get an array now

--- src/ping_pong_mpi.rs
+++ src/ping_pong_mpi.rs
@@ -1,11 +1,12 @@
 use mpi_sys::*;
 use std::os::raw::{c_int, c_void};
-use std::{mem};
+use std::ptr;
+use std::mem::size_of;
 use std::time::Instant;
 
 pub fn ping_pong() {
     // Initialize
-    unsafe { MPI_Init(std::ptr::null_mut(), std::ptr::null_mut()); }
+    unsafe { MPI_Init(ptr::null_mut(), ptr::null_mut()); }
     // Get world size and rank
     let mut world_size: c_int = 0;
     let mut rank: c_int = 0;
@@ -22,56 +23,50 @@ pub fn ping_pong() {
     }
 
     // Start of the main body
-
-    let mut round = 0;
-    // Declaring a window type
-    // let mut window: MPI_Win = unsafe { mem::MaybeUninit::uninit().assume_init() };
-    let mut window = std::ptr::null_mut();
+    let mut master_buf= [0; 5].as_mut_ptr();
+    let mut my_buf = [0; 5].as_mut_ptr();
     // Create a window, not allocate
-    // where you already have a local buffer to expose
-    unsafe {
-        MPI_Win_create(
-            &mut round as *mut c_int as *mut c_void,
-            mem::size_of::<c_int>() as MPI_Aint,
-            1,
-            RSMPI_INFO_NULL,
-            RSMPI_COMM_WORLD,
-            &mut window
-        );
+    // create is you already have an allocated buffer
+    // allocate is you haven't, MPI allocate it for you
+
+    // Displacement unit: simplify access with a single datatype
+    // typical use: either 1 (all access are in terms of byte offset) or sizeof(type)
+    let mut window = ptr::null_mut();
+    if rank == 0 {
+        unsafe { MPI_Win_create(master_buf as *mut c_void, (5 * size_of::<c_int>()) as MPI_Aint,
+                                size_of::<c_int>() as c_int, RSMPI_INFO_NULL, RSMPI_COMM_WORLD, &mut window);
+        }
+    } else {
+        unsafe { MPI_Win_create(ptr::null_mut(), 0, 1,
+                                RSMPI_INFO_NULL, RSMPI_COMM_WORLD, &mut window);
+        }
     }
 
-    // Start of ping pong
-    for i in 0..10 {
-        // Start of a epoch with fence
-        unsafe { MPI_Win_fence(0, window); }
-
-        let data = 1;
-        let sender_rank = i % 2;
-        let receiver_rank = 1 - i % 2;
-
-        let start = Instant::now();
-        if rank == i % 2 {
-            // send with put
-            unsafe {
-                MPI_Put(
-                    &data as *const c_int as *const c_void,
-                    1,
-                    RSMPI_INT32_T,
-                    receiver_rank,
-                    0,
-                    1,
-                    RSMPI_INT32_T,
-                    window
-                );
-            };
-            // println!("i = {}, rank {} -> {} ", i, sender_rank, receiver_rank);
+    if rank == 0 {
+        for i in 0..5 {
+            unsafe { *master_buf.add(i) = 114514 as c_int };
         }
-        // end of a epoch with fence
-        unsafe { MPI_Win_fence(0, window); }
+    }
 
-        if rank != i % 2 {
-            let duration = start.elapsed().as_secs_f64() * 1000.0;
-            println!("sender: {}, receiver: {}, latency: {}", sender_rank, receiver_rank, duration);
+    let sender_rank = 0;
+    let receiver_rank = 1;
+
+    // Start of a epoch with fence
+    unsafe { MPI_Win_fence(0, window); }
+
+    if rank == receiver_rank {
+        unsafe { MPI_Get(my_buf as *mut c_void, 5, RSMPI_INT32_T,
+                         0, 0, 5, RSMPI_INT32_T, window); }
+    }
+
+    unsafe { MPI_Win_fence(0, window); }
+
+    if rank == 1 {
+        for i in 0..5 {
+            unsafe {
+                let val = *(my_buf.wrapping_add(i));
+                println!("{}", val);
+            }
         }
     }
 
@@ -80,5 +75,4 @@ pub fn ping_pong() {
         MPI_Win_free(&mut window);
         MPI_Finalize();
     }
-
 }
