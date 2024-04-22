1cc15d32021c7301f04b5cf4a61871b5b3f5bfaa
Don <iamshixudong@gmail.com>
2024-04-22 23:15:42 +0200

    Refactored test

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use std::env;
+use mpi::topology::Communicator;
+
 mod ping_pong_norm;
 mod ping_pong_rma;
+mod test_utils;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let vec_size = args[1].parse().expect("Failed to parse args[1] as usize");
-    let ping_rounds = args[2].parse().expect("Failed to parse args[2] as usize");
-    let mpi_type: &str = &args[3];
+    // let vec_size = args[1].parse().expect("Failed to parse args[1] as usize");
+    // let ping_rounds = args[2].parse().expect("Failed to parse args[2] as usize");
+    let mpi_type: &str = &args[1];
     match mpi_type {
-        "pin_rma" => ping_pong_rma::ping_pong(vec_size, ping_rounds),
-        "pin_norm" => ping_pong_norm::ping_pong(vec_size, ping_rounds),
+        "rma" => ping_pong_rma::ping_pong(),
+        "norm" => ping_pong_norm::ping_pong(0, 0),
         _ => println!("Invalid argument, run either ping pong | sor_source_data, rma | norm"),
     }
 }
--- src/ping_pong_rma.rs
+++ src/ping_pong_rma.rs
@@ -1,51 +1,45 @@
-use std::ffi::{c_double, c_int, c_void};
-use std::mem::size_of;
-use std::ops::Add;
-use std::ptr;
-use mpi::topology::Communicator;
-use mpi::{ffi, window};
-use mpi::ffi::{MPI_Win, MPI_Win_fence, MPI_Win_free, RSMPI_COMM_WORLD, RSMPI_DOUBLE, RSMPI_INFO_NULL};
-use mpi::window::Window;
-use mpi_sys::MPI_Aint;
+use mpi::Rank;
+use mpi::topology::{Communicator, SimpleCommunicator};
+use mpi::window::{AllocatedWindow, WindowOperations};
+use crate::test_utils::{append_to_csv, generate_test_size};
 
-pub fn ping_pong(vector_size: usize, round_num: usize) {
+pub fn ping_pong() {
     let universe = mpi::initialize().unwrap();
     let world = universe.world();
-    let size = world.size();
     let rank = world.rank();
-
-    let initiator_rank = 0usize;
-    let receiver_rank = 1usize;
-
-    // Start of the main body
-    let mut win = Window::allocate(vector_size);
 
     // **********************
     // * Start of ping pong *
     // **********************
-    let mut test_data = Vec::new();
-    let t_start = mpi::time();
+    let points = generate_test_size(10, 10_000_000, 100);
+    for i in points {
+        run_ping_pong(i, rank, &world);
+    }
+}
+
+fn run_ping_pong(vector_size: usize, rank: Rank, world: &SimpleCommunicator) {
+    let mut win: AllocatedWindow<f64> = world.allocate_window(vector_size);
+    let mut latency_data = Vec::new();
     // each ping pong repeats 10 times
     for _ in 0..10 {
+        let t_start = mpi::time();
         win.fence();
-        if rank == receiver_rank as i32 {
-            win.get_whole_vector(initiator_rank);
+        if rank == 1i32 {
+            win.get_whole_vector(0);
         }
         win.fence();
-        if rank == receiver_rank as i32 {
+        if rank == 1i32 {
             win.window_vector.iter_mut().for_each(|x| *x += 1f64);
-            win.put_whole_vector(initiator_rank);
+            win.put_whole_vector(0);
         }
         win.fence();
-        if rank == initiator_rank as i32 {
-            println!("{:?}", win.window_vector);
+        let t_end = mpi::time();
+        // Collect data
+        if rank == 0i32 {
+            latency_data.push((t_end - t_start) * 1000f64);
         }
     }
-    let t_end = mpi::time();
-    test_data.push((t_end - t_start) / 10f64 * 1000f64);
-
-    if rank == initiator_rank as i32 {
-        println!("Finished {} rounds of ping ping", round_num);
-        println!("Obtained {} results", test_data.len());
+    if rank == 0i32 {
+        append_to_csv("rma_data.csv", vector_size, &latency_data).expect("Failed to write csv");
     }
 }
